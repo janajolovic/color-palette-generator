df5a6f5c94c5ae58564e29ac018ca354e7c9358a
Jana Jolovic <janajolovic04@gmail.com>
2021-09-03 23:46:29 +0200

    generate color

---
+++ colors.js
@@ -0,0 +1,29 @@
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHex = document.querySelectorAll(".color h2");
+let initialColors;
+
+function generateHex() {
+    // letters = "0123456789ABCDEF";
+    // hash = "#";
+    // for (let i = 0; i < 6; i++) {
+    //     hash += letters[Math.floor(Math.random() * 16)]
+    // }
+    // return hash;
+
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        div.style.background = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+
+randomColors();
