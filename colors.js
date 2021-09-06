bdc30fc4703380ef89770d8798ae0f973ac01d75
Jana Jolovic <janajolovic04@gmail.com>
2021-09-06 14:06:10 +0200

    updating slide values

--- colors.js
+++ colors.js
@@ -50,7 +50,9 @@ function randomColors() {
         console.log(sliders)
 
         colorizeSliders(color, hue, brightness, saturation);
+
     });
+    resetInputs();
 }
 
 
@@ -114,5 +116,27 @@ function updateTextUI(index) {
         checkTextContrast(color, icon)
     }
 }
+
+function resetInputs() {
+    const sliders = document.querySelectorAll(".sliders input");
+    sliders.forEach(slider => {
+        if (slider.name === "hue") {
+            const hueColor = initialColors[slider.getAttribute("data-hue")];
+            const hueValue = chroma(hueColor).hsl()[0];
+            slider.value = Math.floor(hueValue);
+        }
+        if (slider.name === "brightness") {
+            const brightColor = initialColors[slider.getAttribute("data-bright")];
+            const brightValue = chroma(brightColor).hsl()[2];
+            slider.value = Math.floor(brightValue * 100) / 100;
+        }
+        if (slider.name === "saturation") {
+            const satColor = initialColors[slider.getAttribute("data-sat")];
+            const satValue = chroma(satColor).hsl()[1];
+            slider.value = Math.floor(satValue * 100) / 100;
+        }
+    })
+}
+
 
 randomColors();
