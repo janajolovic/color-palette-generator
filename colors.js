8b95e35392d8dec45b42ff79139277ace101ee57
Jana Jolovic <janajolovic04@gmail.com>
2021-09-08 12:07:55 +0200

    open close savee

--- colors.js
+++ colors.js
@@ -8,6 +8,8 @@ const adjustButton = document.querySelectorAll(".adjust");
 const closeAdjustments = document.querySelectorAll(".close-adjustment");
 const sliderContainers = document.querySelectorAll(".sliders");
 const lockButton = document.querySelectorAll(".lock");
+let savedPalettes = [];
+
 
 generateBtn.addEventListener("click", randomColors); 
 
@@ -216,5 +218,27 @@ function openAdjustmentPanel(index) {
 function closeAdjustmentPanel(index) {
     sliderContainers[index].classList.remove("active");
 }
+
+const saveBtn = document.querySelector(".save");
+const submitSave = document.querySelector(".submit-save");
+const closeSave = document.querySelector(".close-save");
+const saveContainer = document.querySelector(".save-container");
+const saveInput = document.querySelector(".save-container input");
+
+saveBtn.addEventListener("click", openPalette);
+closeSave.addEventListener("click", closePalette);
+
+function openPalette(e) {
+    const popup = saveContainer.children[0];
+    saveContainer.classList.add("active");
+    popup.classList.add("active");
+}
+
+function closePalette(e) {
+    const popup = saveContainer.children[0];
+    saveContainer.classList.remove("active");
+    popup.classList.remove("active");
+}
+
 
 randomColors();
